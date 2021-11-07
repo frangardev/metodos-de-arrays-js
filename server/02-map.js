0e6fa535e5854d0d3267edaf6e836155f93d8f88
Francisco García <francoffee57@gmail.com>
2021-11-07 13:29:31 -0600

    Ejemplo usando map

---
+++ server/02-map.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3,4,5,6,7,8,9]
+
+const squaredNumbers = numbers.map(item => item ** 2)
+const cubedNumbers = numbers.map(item => item ** 3)
+
+console.log(numbers);
+console.log(squaredNumbers);
+console.log(cubedNumbers);
