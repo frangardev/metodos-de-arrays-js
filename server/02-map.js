88f93f44f540eb35444cae110bc73a09dd011ad0
Francisco García <francoffee57@gmail.com>
2021-11-08 14:41:14 -0600

    Hice un ejemplo usando map y forEach. También hice el ejemplo usando map con objetos

--- server/02-map.js
+++ server/02-map.js
@@ -6,3 +6,14 @@ const cubedNumbers = numbers.map(item => item ** 3)
 console.log(numbers);
 console.log(squaredNumbers);
 console.log(cubedNumbers);
+
+
+for (let index = 0; index < numbers.length; index++) {
+    const squaredNumbers = numbers.map(item => item ** numbers[index])
+    console.log( 'Números al ' + numbers[index] +':     ' + squaredNumbers)
+}
+
+numbers.forEach(item => {
+    const squaredNumbers = numbers.map(item2 => item2 ** item)
+    console.log( 'Números al ' + item +' usando fordEach y map:     ' + squaredNumbers)
+})
