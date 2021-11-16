b3d32fcb66df26982f73b1968c4238154d731691
Francisco García <francoffee57@gmail.com>
2021-11-15 18:32:45 -0600

    Ejemplo de includes

---
+++ server/10-includes.js
@@ -0,0 +1,9 @@
+//Includes nos dice si un elemento forma parte del array
+//Include no recibe una funcion, recibe un parametro
+//Includes tambien funciona para strings 
+
+const pets = ['Cat', 'Dog', 'Bat', 'Rabbit']
+
+const areCat = pets.includes('Cat')
+
+console.log(areCat);
